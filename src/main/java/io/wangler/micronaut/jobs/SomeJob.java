b3116fd6917685ceb82c1c65a3ad571167e9a1a2
Silvio Wangler <71073+saw303@users.noreply.github.com>
2023-07-26 13:02:38 +0200

    Demo code for issues with @Scheduled

---
+++ src/main/java/io/wangler/micronaut/jobs/SomeJob.java
@@ -0,0 +1,19 @@
+package io.wangler.micronaut.jobs;
+
+import io.micronaut.scheduling.annotation.Scheduled;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class SomeJob {
+
+    private boolean paused = false;
+
+    @Scheduled(fixedRate = "${hello.world.rate}", condition = "#{!this.paused}" )
+    public void doSomething() {
+        System.out.println("The job runs...");
+    }
+
+    public boolean isPaused() {
+        return paused;
+    }
+}
